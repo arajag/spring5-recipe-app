9d4da54ad67f858083b6c18556f41fe0c545e353
acrajag <andre.raja@gmail.com>
2021-10-09 23:03:47 +0100

    :sparkles: Create index page

--- src/main/java/guru/springframework/DemoApplication.java
+++ src/main/java/guru/springframework/DemoApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework.demo;
+package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
