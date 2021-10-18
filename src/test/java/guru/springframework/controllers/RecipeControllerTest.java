1e1bd0d67bfaec9d6f3a1be50cd46c120abe9195
acrajag <andre.raja@gmail.com>
2021-10-18 08:29:07 +0100

    :sparkles: 194. Display a Recipe by ID (I did using new versions of JUnit, SpringMvc and Mockito).

---
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -0,0 +1,54 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+/**
+ * Created by jt on 6/19/17.
+ */
+@ExtendWith(MockitoExtension.class)
+public class RecipeControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @InjectMocks
+    RecipeController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void Setup() {
+         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+
+    @Test
+    public void testGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"));
+    }
+}
